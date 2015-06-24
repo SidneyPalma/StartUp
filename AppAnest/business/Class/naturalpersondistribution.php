33d90ff066ba9dcdcb16eed89d959cfb849e7039
Samuel Oliveira <samuca95@hotmail.com>
2015-06-23 22:41:00 -0300

    Criação da Distribuição dos Plantões - Sócios

--- AppAnest/business/Class/naturalpersondistribution.php
+++ AppAnest/business/Class/naturalpersondistribution.php
@@ -1,6 +1,10 @@
 <?php
 
 require_once '../../vendor/autoload.php';
+//
+//$_POST['action'] = 'select';
+//$_POST['method'] = 'selectCode';
+//$_POST['naturalpersonid'] = 351;
 
 $object = new \AppAnest\Coach\naturalpersondistribution();
 

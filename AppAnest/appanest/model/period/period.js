46651d2940fd24002d56b7c2e090afc6979de6ff
Samuel Oliveira <samuca95@hotmail.com>
2015-07-28 09:38:05 -0300

    Remove and Fix Classes

--- AppAnest/appanest/model/period/period.js
+++ AppAnest/appanest/model/period/period.js
@@ -12,10 +12,22 @@ Ext.define( 'AppAnest.model.period.Period', {
             name: 'month',
             type: 'int'
         }, {
+            name: 'periodof',
+            type: 'auto',
+            convert: function (value, record) {
+                return Ext.util.Format.date(Ext.Date.parse(value,"Y-m-d"),'d/m/Y');
+            }
+        }, {
+            name: 'periodto',
+            type: 'auto',
+            convert: function (value, record) {
+                return Ext.util.Format.date(Ext.Date.parse(value,"Y-m-d"),'d/m/Y');
+            }
+        }, {
             name: 'description',
             type: 'auto',
-            convert: function (value,record) {
-                return Ext.String.leftPad(record.get('year'), 4, '0') +'/' + Ext.String.leftPad(record.get('month'), 2, '0');
+            convert: function (value, record) {
+                return Ext.String.leftPad(record.get('year'), 4, '0') + '/' + Ext.String.leftPad(record.get('month'), 2, '0');
             }
         }
     ]
